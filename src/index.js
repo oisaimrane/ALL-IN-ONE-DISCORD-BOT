74949479574c669a19ea544044b6a681f75f4ffe
oisaimrane <95139857+oisaimrane@users.noreply.github.com>
2024-12-05 22:10:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 const Discord = require('discord.js');
 const chalk = require('chalk');
 require('dotenv').config('./.env');
